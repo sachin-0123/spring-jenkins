daf5aba1a81e324ba5a400238ad6a7fa98611160
sachin-0123 <sachinpandit7561@gmail.com>
2024-07-23 23:55:56 +0530

    error gone

--- src/test/java/com/jenkins/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -17,7 +17,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public  contextLoads() {
+	public  void contextLoads() {
 		logger.info("Test cases executing...");
 		logger.info("Second test cases are executing");
 		logger.info("Hii");
